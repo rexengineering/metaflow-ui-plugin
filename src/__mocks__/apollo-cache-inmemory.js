5a18c7f38cf3112774e81e43bdcc7ae3240447e2
Meta Engineer <m@plankc.com>
2021-07-19 13:42:13 -0500

    WIP

--- src/__mocks__/apollo-cache-inmemory.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  InMemoryCache: jest.fn().mockImplementation(() => ({})),
-};
--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const BUSY = "BUSY";
-export const OFFLINE = "OFFLINE";
-export const ONLINE = "ONLINE";
-
-export const ActionItemDeleteType = "ActionDeleteType";
-export const ActionItemCheckType = "ActionCheckType";
-export const ActionItemDisplayOnlyType = "ActionDisplayOnlyType";
