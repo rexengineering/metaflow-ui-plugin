e6b3c32e13c6fdaa35438edad042b990660a50bb
Meta Engineer <m@plankc.com>
2021-08-01 18:09:38 -0500

    WIP

--- src/RexflowPlugin.js
+++ src/RexflowPlugin.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { VERSION } from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
-import CustomTheme from "./theme";
-import App from "prism";
+import rexFlowReducer, { namespace } from "./states";
+import {Provider} from "react-redux";
+import getStore from "./prism/src/store";
+import { ThemeProvider } from "@material-ui/core/styles";
+import buildTheme from "./prism/src/theme";
+import App from "./prism/src/containers/App";
+import {Button} from "@material-ui/core";
+import {setIsFlexTaskActive} from "./prism/src/store/actions";
 
-const PLUGIN_NAME = 'RexflowPlugin';
-import reducers, { namespace } from './states';
+const PLUGIN_NAME = 'RexFlowPlugin';
 
 export default class RexflowPlugin extends FlexPlugin {
 
@@ -13,16 +18,27 @@ export default class RexflowPlugin extends FlexPlugin {
     super(PLUGIN_NAME);
   }
 
-  init(flex, {store, updateConfig, workerClient, }) {
+  init(flex, {store, workerClient}) {
     const { dispatch } = store;
     this.registerReducers(store);
-    updateConfig(CustomTheme);
-
+    const rexFlowStore = getStore();
+    const prismUITheme = buildTheme();
     const options = { sortOrder: -1 };
     flex.AgentDesktopView
       .Panel2
       .Content
-      .replace(<App dispatch={dispatch}/>, options);
+        .replace(
+
+              <ThemeProvider key="someRandKey" theme={prismUITheme}>
+                <App>
+                  <Button onClick={() => {
+                    dispatch(setIsFlexTaskActive(false));
+                    console.log("Dispatched");
+                  }}> Send call </Button>
+                </App>
+              </ThemeProvider>
+
+            , options);
 
     // Events from Twilio Flex app
     workerClient.on("reservationCreated", () => {});
@@ -36,6 +52,6 @@ export default class RexflowPlugin extends FlexPlugin {
       console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${VERSION}`);
       return;
     }
-    store.addReducer(namespace, reducers);
+    store.addReducer(namespace, rexFlowReducer);
   }
 }
--- src/states.js
+++ src/states.js
@@ -1,4 +1,4 @@
-import rexFlowReducer from "prism/store/reducers/rexflow";
+import rexFlowReducer from "./prism/src/store/reducers/rexflow";
 
 export const namespace = 'rexflow';
 export default rexFlowReducer;
