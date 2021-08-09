6e598edf38768446c35307a0a20919465b0476f9
Meta Engineer <m@plankc.com>
2021-08-09 14:58:53 -0500

    Adding resolvers for SVG files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 module.exports = (config, { isProd, isDev, isTest }) => {
-  /**
-   * Customize the webpack by modifying the config object.
-   * Consult https://webpack.js.org/configuration for more information
-   */
-
-  return config;
+  const { module: { rules } } = config;
+  const svgLoader = [
+    {
+      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      loader: 'url-loader?limit=100000'
+    }
+  ];
+  return {
+    ...config,
+    module: {
+      rules: [
+        ...rules,
+        ...svgLoader
+      ]
+    }
+  };
 }
